3e71e33ed9ca14b9ce427ae0e8a1e939ac6fc365
matai <matai@BOZZA>
2024-10-01 21:40:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enkel_miniräknare
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enkel kalkylator");
+        Console.WriteLine("Välj en operator (+, -, /, *)");
+        char operation = Console.ReadKey().KeyChar;
+        Console.WriteLine("Ange första talet: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Ange andra talet: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+
+        double result = 0;
+        switch (operation)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num2 - num1;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num2 * num1;
+                break;
+
+            default:
+                Console.WriteLine("Ogiltig operation");
+                return;
+        }
+
+        Console.WriteLine($"Resultat: {result}");
+        Console.ReadKey();
+    }
+}
